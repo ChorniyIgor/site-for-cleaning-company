2ea975dbe54e53640048f7c2d2b8f2426bbe9bc3
ChorniyIgor <igor.chorniy@ukr.net>
2019-03-26 21:23:21 +0200

    add main page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 let isDevelopment = true;
 
 const gulp = require("gulp");
+const rename = require("gulp-rename");
+const inject = require("gulp-inject-string");
 
 function lazyRequireTask(taskName, path, options = {}) {
   options.taskName = taskName;
@@ -49,8 +51,28 @@ gulp.task("watch", function() {
   gulp.watch("src/js/**/*.js", gulp.series("scripts"));
 });
 
+function someLogicToGetThisValue() {
+  return Math.random() * 1000;
+}
+
+var myHash = someLogicToGetThisValue();
+
+gulp.task("CSSHash", function() {
+  return gulp
+    .src("build/css/style.css")
+    .pipe(rename("style." + myHash + ".css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("htmlInject", function() {
+  return gulp
+    .src("build/index.html")
+    .pipe(inject.replace("style.css", "style." + myHash + ".css"))
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("copy-all", gulp.parallel("copy-html", "copy-assets", "copy-img", "styles", "scripts"));
 
-gulp.task("build", gulp.series("clean", "copy-all"));
+gulp.task("build", gulp.series("clean", "copy-all", "CSSHash", "htmlInject"));
 
 gulp.task("dev", gulp.series("copy-all", gulp.parallel("watch", "server")));
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,20 @@
 import mobHeader from "./header/mobile-header";
 import smallHeader from "./header/small-header";
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
+
+const slider = tns({
+  container: ".partners__list",
+  items: 1,
+  slideBy: "page",
+  autoplay: true,
+  mouseDrag: true,
+  controls: false,
+  responsive: {
+    700: {
+      items: 2
+    },
+    1200: {
+      items: 3
+    }
+  }
+});
